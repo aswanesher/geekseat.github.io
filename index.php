2f8e966a256a2fea402f82dd354592e4043518d4
aswanesher <aswansetiawan01@gmail.com>
2022-08-24 09:40:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,44 @@
 <?php 
 function getYear($jumlah)
 {
-    $yearOfDeath = 0;
-    $ageOfDeath = 1;
+    $intA = 0;
+    $intB = 1;
 
-    $hasil = $ageOfDeath;
+    $hasil = $intB;
 
-    for($i = 0; $i<=$jumlah - 1; $i++)
+    for($i = 0; $i<$jumlah - 1; $i++)
     {
-        $output = $yearOfDeath + $ageOfDeath;
+        $output = ($intA + $intB) + 1;
         $hasil = $hasil." $output";
 
-        $yearOfDeath = $ageOfDeath;
-        $ageOfDeath = $output;
+        $intA = $intB;
+        $intB = $output;
     }
 
     return $hasil;
 }
 
-function witchFibonacci($kill)
+function witchFibonacci($yearOfDeath, $ageOfDeath)
 {
-    for ($i=1; $i < $kill+1 ; $i++) { 
+    $hasil = $yearOfDeath - $ageOfDeath;
+
+    for ($i=1; $i < $hasil+1 ; $i++) { 
         //echo $i;
         echo getYear($i);
         echo "<br>";
     }
 }
 
-witchFibonacci(10);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $yearofdeath = $_POST["yearofdeath"];
+    $ageofdeath = $_POST["ageofdeath"];
+
+    witchFibonacci($yearofdeath,$ageofdeath);
+}
 ?>
+<br><br>
+<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+Year of Death : <input type="text" name="yearofdeath"><br>
+Age of Death : <input type="text" name="ageofdeath"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
